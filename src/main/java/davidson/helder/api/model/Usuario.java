c62febd8a7cf115682e6595ef0047cb35d140c4a
uken49 <heldergavioes@hotmail.com>
2023-03-14 12:42:20 -0300

    refactor: DTO virou Nested Class

--- src/main/java/davidson/helder/api/model/Usuario.java
+++ src/main/java/davidson/helder/api/model/Usuario.java
@@ -2,12 +2,88 @@ package davidson.helder.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
     private final int id;
     private String nome;
     private String senha;
     private String email;
     private boolean contaAutenticada;
+
+    public static class Lista {
+
+        private static List<Usuario> listaUsuarios = new ArrayList<>();
+
+        public static void addUsuario(Usuario usuario) {
+            if (usuarioExiste(usuario.getId())) return;
+            listaUsuarios.add(usuario);
+        }
+
+        public static void removeUsuario(int id) {
+            if (!usuarioExiste(id)) return;
+
+            for (Usuario indexUsuario : listaUsuarios) {
+                if (indexUsuario.getId() != id) continue;
+
+                listaUsuarios.remove(indexUsuario);
+                return;
+            }
+        }
+
+        private static boolean usuarioExiste(int id) {
+            for (Usuario indexUsuario : listaUsuarios) {
+                if (indexUsuario.getId() != id) continue;
+
+                return true;
+            }
+            return false;
+        }
+
+        public static List<Usuario> getListaUsuarios() {
+            return listaUsuarios;
+        }
+
+        public static Usuario getUsuario(int id){
+
+            if (!usuarioExiste(id)) return null;
+
+            for (Usuario indexUsuario:
+                    listaUsuarios) {
+
+                if (indexUsuario.getId() != id) continue;
+
+                return indexUsuario;
+            }
+
+            return null;
+        }
+
+    }
+
+    public static class Cadastro {
+        private final int id;
+        private final String nome;
+        private final String senha;
+        private final String email;
+
+        public Cadastro(Usuario usuario) {
+            this.id = usuario.getId();
+            this.nome = usuario.getNome();
+            this.senha = usuario.getSenha();
+            this.email = usuario.getEmail();
+        }
+
+        public static void cadastrarUsuario(Cadastro novoUsuario){
+            Lista.addUsuario(new Usuario(
+                    novoUsuario.id
+                    , novoUsuario.nome
+                    , novoUsuario.senha
+                    , novoUsuario.email
+            ));
+        }
+    }
 
     public Usuario(int id, String nome, String senha, String email) {
         this.id = id;
--- src/test/java/davidson/helder/api/service/UsuarioTest.java
+++ src/test/java/davidson/helder/api/service/UsuarioTest.java
@@ -10,53 +10,51 @@ import davidson.helder.api.model.Usuario;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UsuarioTest {
+class UsuarioTest {
 
     @BeforeEach
-    public void limparListaECadastrarUsuario(){
-        List<Usuario> listaUsuario = UsuarioLista.getListaUsuarios();
+    void limparListaECadastrarUsuario() {
+        List<Usuario> listaUsuario = Usuario.Lista.getListaUsuarios();
         listaUsuario.clear();
-        
-        assertEquals(0, listaUsuario.size());
-        
 
-        UsuarioCadastro usuario = new UsuarioCadastro(
-            new Usuario(0, "helder", "123", "email")
+        assertEquals(0, listaUsuario.size());
+
+
+        Usuario.Cadastro usuario = new Usuario.Cadastro(
+                new Usuario(0, "helder", "123", "helder@gmail.com")
         );
 
-        usuario.cadastrarUsuario(usuario);
+        Usuario.Cadastro.cadastrarUsuario(usuario);
         assertEquals(1, listaUsuario.size());
     }
-   
+
     @Test
     @DisplayName("Cadastro de usuário")
-    public void cadastrarUsuario() {
-        UsuarioCadastro usuario = new UsuarioCadastro(
-            new Usuario(1, "joao", "@321", "email")
+    void cadastrarUsuario() {
+        Usuario.Cadastro usuario = new Usuario.Cadastro(
+                new Usuario(1, "joao", "123", "joao@bobo.com")
         );
 
-        usuario.cadastrarUsuario(usuario);
+        Usuario.Cadastro.cadastrarUsuario(usuario);
+        assertEquals(2, Usuario.Lista.getListaUsuarios().size());
 
-        usuario.cadastrarUsuario(usuario);
-        assertEquals(2, UsuarioLista.getListaUsuarios().size());
-
-        usuario.cadastrarUsuario(usuario);
-        assertEquals(2, UsuarioLista.getListaUsuarios().size());
+        Usuario.Cadastro.cadastrarUsuario(usuario);
+        assertEquals(2, Usuario.Lista.getListaUsuarios().size());
     }
 
     @Test
     @DisplayName("Exibir um usuário")
-    public void exibirUmUsuario(){
-        List<Usuario> listaUsuario = UsuarioLista.getListaUsuarios();
+    void exibirUmUsuario() {
+        List<Usuario> listaUsuario = Usuario.Lista.getListaUsuarios();
 
-        final int id = UsuarioLista.getUsuario(0).getId();
-        final String nome = UsuarioLista.getUsuario(0).getNome();
-        final String email = UsuarioLista.getUsuario(0).getEmail();
-        final boolean contaAutenticada = UsuarioLista.getUsuario(0).getContaAutenticada();
+        final int id = Usuario.Lista.getUsuario(0).getId();
+        final String nome = Usuario.Lista.getUsuario(0).getNome();
+        final String email = Usuario.Lista.getUsuario(0).getEmail();
+        final boolean contaAutenticada = Usuario.Lista.getUsuario(0).getContaAutenticada();
 
         assertEquals(0, id);
         assertEquals("helder", nome);
-        assertEquals("email", email);
+        assertEquals("helder@gmail.com", email);
         assertFalse(contaAutenticada);
 
     }
